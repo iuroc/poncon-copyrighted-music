b6994d59620e4f653d1d99e4fd7f3f0cf73cdd32
ponconsoft <ponconsoft@gmail.com>
2022-05-05 03:15:51 +0800

    【完成】获取文件下载链接API

--- api/getDownloadUrl.php
+++ api/getDownloadUrl.php
@@ -18,7 +18,13 @@ if (!$fileId) {
 }
 
 // 判断文件是否存在
-$result = mysqli_query($conn, "SELECT `fileId` FROM `copyrighted_music` WHERE `fileId` = '$fileId' LIMIT 1;");
+$result = mysqli_query($conn, "SELECT * FROM `copyrighted_music` WHERE `fileId` = '$fileId' LIMIT 1;");
+if (!$result) {
+    die(json_encode(array(
+        'code' => 903,
+        'msg' => '数据库出错'
+    )));
+}
 if (!mysqli_num_rows($result)) {
     die(json_encode(array(
         'code' => 901,
@@ -29,5 +35,56 @@ if (!mysqli_num_rows($result)) {
 // 判断数据库的下载连接是否过期
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
     $url_update_time = (int)($row['url_update_time']);
-    echo $url_update_time;
+    if (time() - $url_update_time < $config['download_url_update_duration']) {
+        // 链接还在有效期内
+        echo json_encode(array(
+            'code' => 200,
+            'msg' => '查询成功(缓存)',
+            'result' => $row['downloadUrl']
+        ));
+    } else {
+        // 链接过期，重新获取
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => "content-type: application/json\nauthorization: " . $config['123pan']['authorization'],
+                'content' => json_encode(array(
+                    "driveId" => 0,
+                    "etag" => $row['etag'],
+                    "fileId" => $row['fileId'],
+                    "s3keyFlag" => $row['s3keyFlag'],
+                    "type" => 0,
+                    "fileName" => $row['fileName'],
+                    "size" => $row['size']
+                )),
+                'timeout' => 900
+            ),
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            )
+        );
+        $context = stream_context_create($options);
+        $result2 = file_get_contents('https://www.123pan.com/a/api/file/download_info', false, $context);
+        $download_url = json_decode($result2, true)['data']['DownloadUrl'];
+        if (!$download_url) {
+            die(json_encode(array(
+                'code' => 902,
+                'msg' => '服务器出错'
+            )));
+        }
+        // 更新数据库下载链接
+        $result3 = mysqli_query($conn, "UPDATE `copyrighted_music` SET `downloadUrl` = '$download_url', `url_update_time` = " . time() . " WHERE `fileId` = '$fileId' LIMIT 1;");
+        if (!$result3) {
+            die(json_encode(array(
+                'code' => 903,
+                'msg' => '数据库出错'
+            )));
+        }
+        echo json_encode(array(
+            'code' => 200,
+            'msg' => '查询成功(非缓存)',
+            'result' => $download_url
+        ));
+    }
 }
