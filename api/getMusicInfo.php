d0dd1dba14a68a1610869a5e18b8dffc2ba3c96a
欧阳鹏 <61752998+oyps@users.noreply.github.com>
2022-07-26 11:33:48 +0800

    Update getMusicInfo.php

--- api/getMusicInfo.php
+++ api/getMusicInfo.php
@@ -44,61 +44,68 @@ function addListenNum($conn, $listen_num, $fileId)
 
 // 判断数据库的下载连接是否过期
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    $url_update_time = (int)($row['url_update_time']);
-    if (time() - $url_update_time < $config['download_url_update_duration']) {
-        // 链接还在有效期内
-        echo json_encode(array(
-            'code' => 200,
-            'msg' => '查询成功(缓存)',
-            'result' => $row
-        ));
-        addListenNum($conn, (int)($row['listen_num']), $fileId);
-    } else {
-        // 链接过期，重新获取
-        $options = array(
-            'http' => array(
-                'method' => 'POST',
-                'header' => "content-type: application/json\nauthorization: " . $config['123pan']['authorization'],
-                'content' => json_encode(array(
-                    "driveId" => 0,
-                    "etag" => $row['etag'],
-                    "fileId" => $row['fileId'],
-                    "s3keyFlag" => $row['s3keyFlag'],
-                    "type" => 0,
-                    "fileName" => $row['fileName'],
-                    "size" => $row['size']
-                )),
-                'timeout' => 900
-            ),
-            "ssl" => array(
-                "verify_peer" => false,
-                "verify_peer_name" => false,
-            )
-        );
-        $context = stream_context_create($options);
-        $result2 = file_get_contents('https://www.123pan.com/a/api/file/download_info', false, $context);
-        $download_url = json_decode($result2, true)['data']['DownloadUrl'];
-        if (!$download_url) {
-            die(json_encode(array(
-                'code' => 902,
-                'msg' => '服务器出错'
-            )));
-        }
-        // 更新数据库下载链接
-        $download_url = addslashes($download_url);
-        $result3 = mysqli_query($conn, "UPDATE `copyrighted_music` SET `downloadUrl` = '$download_url', `url_update_time` = " . time() . " WHERE `fileId` = '$fileId' LIMIT 1;");
-        if (!$result3) {
-            die(json_encode(array(
-                'code' => 903,
-                'msg' => '数据库出错，' . mysqli_error($conn)
-            )));
-        }
-        $row['downloadUrl'] = $download_url;
-        echo json_encode(array(
-            'code' => 200,
-            'msg' => '查询成功(非缓存)',
-            'result' => $row
-        ));
-        addListenNum($conn, (int)($row['listen_num']), $fileId);
-    }
+    $row['downloadUrl'] = 'http://123pan.ouyangpeng.top/FreePD/' . $row['fileName'];
+    echo json_encode(array(
+        'code' => 200,
+        'msg' => '查询成功(缓存)',
+        'result' => $row
+    ));
+    addListenNum($conn, (int)($row['listen_num']), $fileId);
+    // $url_update_time = (int)($row['url_update_time']);
+    // if (time() - $url_update_time < $config['download_url_update_duration']) {
+    //     // 链接还在有效期内
+    //     echo json_encode(array(
+    //         'code' => 200,
+    //         'msg' => '查询成功(缓存)',
+    //         'result' => $row
+    //     ));
+    //     addListenNum($conn, (int)($row['listen_num']), $fileId);
+    // } else {
+    //     // 链接过期，重新获取
+    //     $options = array(
+    //         'http' => array(
+    //             'method' => 'POST',
+    //             'header' => "content-type: application/json\nauthorization: " . $config['123pan']['authorization'],
+    //             'content' => json_encode(array(
+    //                 "driveId" => 0,
+    //                 "etag" => $row['etag'],
+    //                 "fileId" => $row['fileId'],
+    //                 "s3keyFlag" => $row['s3keyFlag'],
+    //                 "type" => 0,
+    //                 "fileName" => $row['fileName'],
+    //                 "size" => $row['size']
+    //             )),
+    //             'timeout' => 900
+    //         ),
+    //         "ssl" => array(
+    //             "verify_peer" => false,
+    //             "verify_peer_name" => false,
+    //         )
+    //     );
+    //     $context = stream_context_create($options);
+    //     $result2 = file_get_contents('https://www.123pan.com/a/api/file/download_info', false, $context);
+    //     $download_url = json_decode($result2, true)['data']['DownloadUrl'];
+    //     if (!$download_url) {
+    //         die(json_encode(array(
+    //             'code' => 902,
+    //             'msg' => '服务器出错'
+    //         )));
+    //     }
+    //     // 更新数据库下载链接
+    //     $download_url = addslashes($download_url);
+    //     $result3 = mysqli_query($conn, "UPDATE `copyrighted_music` SET `downloadUrl` = '$download_url', `url_update_time` = " . time() . " WHERE `fileId` = '$fileId' LIMIT 1;");
+    //     if (!$result3) {
+    //         die(json_encode(array(
+    //             'code' => 903,
+    //             'msg' => '数据库出错，' . mysqli_error($conn)
+    //         )));
+    //     }
+    //     $row['downloadUrl'] = $download_url;
+    //     echo json_encode(array(
+    //         'code' => 200,
+    //         'msg' => '查询成功(非缓存)',
+    //         'result' => $row
+    //     ));
+    //     addListenNum($conn, (int)($row['listen_num']), $fileId);
+    // }
 }
